da149517f43734566f5c82581d993395c6b970ad
Gopal_Krushna <96003295+gopal15092La@users.noreply.github.com>
2023-12-10 16:14:35 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+// storing some info. of an USER in memory space
+/*
+    prefer not to use "var"
+    beacause issue in 'Block scope' & 'functional scope'
+*/
+const accountId = 123456
+let accountEmail = "Gopal@gmail.com"
+var accountPassword = "Gopal123@"
+let accountState 
+// node 01_basics/01_variables.js
+accountCity = "Delhi"
+
+// accountId = 34 {varibles with "const" keyword can not be updated }
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState ])
+accountEmail = "bapo@gmail.com"
+accountPassword = "Bapo123@"
+accountCity = "ganjam"
+accountState  ="Odisha"
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState ])
