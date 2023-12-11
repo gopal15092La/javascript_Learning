f7c997a9a70a69ba6fa628faf739c0efc1dee467
Gopal_Krushna <96003295+gopal15092La@users.noreply.github.com>
2023-12-11 21:37:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(5+8) we are using node.js, not browser
+
+console.log(3
+    +
+    2) // Code Readability should be HIGH ***
+
+// DataType examples and DataTypes
+let name = "Hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// let x;
+// console.log(x);
+// x = null
+// console.log(x);
+
+
+// datatypes are :-
+
+// number => upto 2 to power 53
+// bigint => for large value storing
+// string => ""
+// boolean => true/false
+
+// null => Stand ALone value{An OBJECT} ***imp***
+
+// undefined => varibale declared, but no values assigned to it
+
+// symbol => unique catagorization ke liye
+
+// object
+
+console.log(typeof "Gopal")
+console.log(typeof 233)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
