1db829d9f817617056540d139e0968939f256292
Gopal_Krushna <96003295+gopal15092La@users.noreply.github.com>
2024-02-11 18:30:45 +0000

    dates commit

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,4 @@
+// path - 01_basics/07_dates.js
+// *)Note - Javascript "Date" object represents a single moment in time in a platform-independent format.
+// Date object encapsulates an integral number that represents milliseconds 
+// since the midnight at the beginning of " January -01 - 1970"
