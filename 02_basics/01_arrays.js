f9aec330b77fd7f37ab53e1583f51efd8b3ce786
Gopal_Krushna <96003295+gopal15092La@users.noreply.github.com>
2024-02-16 20:34:45 +0000

    array commit

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+// path -> node 02_basics/01_arrays.js
+
+// array
+// (*) the "Array" object 
+
+let arr = [0 ,1 ,5, 2, 4 , 3]
+
+// console.log(arr.length);
+
+arr.push("Gopal");
+// console.log(arr)
+arr.pop();
+
+
+arr.unshift("Gopal");
+// console.log(arr)
+// arr.shift();
+// console.log(arr)
+// console.log(arr[arr.length - 1]);
+
+console.log(arr.includes("Gop"));
+console.log(arr.indexOf(4));
+console.log(arr);
+
